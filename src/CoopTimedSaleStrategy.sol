c5747eb247670e4d1c08f33f18ee8b38a3768b21
Sweets Sweetman <sweetmantech@gmail.com>
2024-10-09 19:52:25 -0300

    I add missing author.

--- src/CoopTimedSaleStrategy.sol
+++ src/CoopTimedSaleStrategy.sol
@@ -28,7 +28,7 @@ import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.s
 
 /// @title Zora Timed Sale Strategy
 /// @notice A timed sale strategy for Zora 1155 tokens
-/// @author @isabellasmallcombe @kulkarohan
+/// @author @sweetman @isabellasmallcombe @kulkarohan
 contract CoopTimedSaleStrategy is ERC1967Proxy {
     constructor(address _logic) ERC1967Proxy(_logic, "") {}
 }
